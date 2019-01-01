8577c5a67cf3548e1676a0eab3a9b15c8449e529
cashenvic <comptes.kh@protonmail.com>
2019-01-01 20:52:12 +0100

    creation du component de dialog d'ajout de patient

---
+++ src/app/patient-add-form/patient-add-form.component.ts
@@ -0,0 +1,42 @@
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
+import {PatientInterface} from '../dataInterfaces/patient';
+import {NgForm} from '@angular/forms';
+import {MAT_DIALOG_DATA, MatDialogRef, MatSnackBar} from '@angular/material';
+import {AffectationPatientDialogData} from "../modify-patient/modify-patient.component";
+
+
+@Component({
+    selector: 'app-patientaddform',
+    templateUrl: './patient-add-form.component.html',
+    styleUrls: ['./patient-add-form.component.css']
+})
+export class PatientAddFormComponent implements OnInit {
+    @ViewChild('formulaire') formulaire: NgForm;
+    sexe: string;
+
+    patient: PatientInterface = {
+        prenom: '',
+        nom: '',
+        sexe: null,
+        numeroSecuriteSociale: '',
+        adresse: {
+            ville: '',
+            codePostal: null,
+            rue: '',
+            numero: '',
+            etage: '',
+        }
+    };
+
+    constructor(public snackBar: MatSnackBar,
+                public dialogRef: MatDialogRef<PatientAddFormComponent>,
+                @Inject(MAT_DIALOG_DATA) public data: AffectationPatientDialogData) {
+    }
+
+    ngOnInit() {
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+}
