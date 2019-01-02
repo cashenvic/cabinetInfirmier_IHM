ce9c901c50afcf9009abe9d433a548d7b9a4e3b6
cashenvic <comptes.kh@protonmail.com>
2019-01-02 12:40:33 +0100

    ajout de tooltip aux boutons

--- src/app/profil-inf/profil-inf.component.ts
+++ src/app/profil-inf/profil-inf.component.ts
@@ -14,42 +14,45 @@ import {CabinetMedicalService} from "../services/cabinet-medical.service";
 })
 export class ProfilInfComponent implements OnInit {
     @Input() infirmier: InfirmierInterface;
-  imgSrc: string;
+    imgSrc: string;
     patients: PatientInterface[];
     actesMedical: ActeInterface;
     infirmiers: InfirmierInterface[];
 
-  panelOpenState = false;
-  
-  affecter = false;
-  desaffecter = false;
+    panelOpenState = false;
+
+    affecter = false;
+    desaffecter = false;
 
     constructor(private actesService: ActeMedicalService, private authService: AuthService, private cabinetService: CabinetMedicalService) {
+
+    }
+
+    ngOnInit() {
+        const person = Log.secretaire;
+        this.authService.verifLog(person);
+
         this.cabinetService.getInfirmiers('/data/cabinetInfirmier.xml').then((inf) => {
             this.infirmiers = inf;
         });
-  }
-  ngOnInit() {
-    const person = Log.secretaire;
-    this.authService.verifLog(person);
 
-    this.actesService.getDataActe('/data/actes.xml').then(actes => {
-      this.actesMedical = actes;
-        //console.log(this.actesMedical.types);
-    });  
+        this.actesService.getDataActe('/data/actes.xml').then(actes => {
+            this.actesMedical = actes;
+            //console.log(this.actesMedical.types);
+        });
 
-    this.imgSrc = 'data/' + this.infirmier.photo;
-    this.patients = this.infirmier.patients;
-  }
+        this.imgSrc = 'data/' + this.infirmier.photo;
+        this.patients = this.infirmier.patients;
+    }
 
-  
-  affecterPat(){
-    this.affecter=true;
-  }
 
-  desaffecterPat(){
-    this.desaffecter=true;
-  }
+    affecterPat() {
+        this.affecter = true;
+    }
+
+    desaffecterPat() {
+        this.desaffecter = true;
+    }
 
     getInfirmiers() {
 
