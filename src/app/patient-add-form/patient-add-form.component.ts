e4ab47a78c6262ec1f100c20c8267fc24bb4d125
cashenvic <comptes.kh@protonmail.com>
2019-01-03 15:28:32 +0100

    pattern pour les input add patient et recup valeur sexe

--- src/app/patient-add-form/patient-add-form.component.ts
+++ src/app/patient-add-form/patient-add-form.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, NgModule, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {MAT_DATE_LOCALE, MAT_DIALOG_DATA, MatDialogRef, MatSnackBar} from '@angular/material';
 import {AffectationPatientDialogData} from "../modify-patient/modify-patient.component";
+import {sexeEnum} from "../dataInterfaces/sexe";
 
 @NgModule({
     providers: [
@@ -25,6 +26,11 @@ export class PatientAddFormComponent implements OnInit {
     constructor(public snackBar: MatSnackBar,
                 public dialogRef: MatDialogRef<PatientAddFormComponent>,
                 @Inject(MAT_DIALOG_DATA) public data: AffectationPatientDialogData) {
+        if (data.patient.sexe === sexeEnum.F) {
+            this.data.patient.sexe = 'F';
+        } else {
+            this.data.patient.sexe = 'M';
+        }
     }
 
     ngOnInit() {
